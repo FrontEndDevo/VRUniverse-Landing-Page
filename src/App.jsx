4a2aba86635ae5e6d21434e304a615e07a47ae80
FrontEndDevo <mylinkacco@gmail.com>
2023-05-02 16:36:24 +0300

    Clean the app before start creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import classes from "./App.module.scss";
-
 const App = () => {
-  return (
-    <div className={classes.text}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sequi
-      necessitatibus accusantium, saepe quia dolore. Odit fugit vel mollitia,
-      quia labore ullam doloremque eveniet nostrum? Distinctio harum aliquid
-      maxime suscipit ipsa ratione magnam officia quo molestias fugit,
-      perspiciatis soluta expedita nihil corrupti voluptates quis eveniet eaque?
-      A quos esse quaerat consequuntur sunt minima! Deserunt id possimus totam,
-      quis perspiciatis eum, incidunt doloremque ipsum necessitatibus veniam
-      voluptatibus aliquid atque eius quibusdam placeat recusandae ducimus
-      corrupti aliquam distinctio voluptatem laboriosam? Commodi ducimus
-      eligendi autem voluptatibus est inventore accusantium ipsum minima? Totam
-      quo obcaecati earum neque mollitia quasi hic adipisci possimus autem est.
-    </div>
-  );
+  return <>
+    
+  </>;
 };
 
 export default App;
