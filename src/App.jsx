bd75a0bca4546b6fbbbb079d362ebe950d7ef95c
FrontEndDevo <mylinkacco@gmail.com>
2023-05-09 13:22:15 +0300

    Create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About/About";
+import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <About />
+      <Header />
+      {/* <About /> */}
     </>
   );
 };
